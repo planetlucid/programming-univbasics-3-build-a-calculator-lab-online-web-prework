1b4adf3d2006cd4238df4352100c8910ecf44329
restful-strategy-4685 <planetlucid@gmail.com>
2019-08-02 18:12:44 +0000

    Automatically backed up by Learn

--- lib/math.rb
+++ lib/math.rb
@@ -9,15 +9,17 @@ end
 subtraction (num1,num2)
 
 def division(num1, num2)
-subtraction == num1 / num2
+division == num1 / num2
 end
+division (num1,num2)
 
 def multiplication(num1, num2)
-subtraction == num1 / num2
+multiplication == num1 / num2
 end
+multiplication (num1,num2)
 
 def modulo(num1, num2)
-
+modulo
 end
 
 def square_root(num)
