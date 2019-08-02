338a46c49a98f6ccc25f6cb4e17d57e73e7a851e
restful-strategy-4685 <planetlucid@gmail.com>
2019-08-02 18:30:55 +0000

    Automatically backed up by Learn

--- lib/math.rb
+++ lib/math.rb
@@ -1,29 +1,23 @@
 def addition(num1, num2)
-addition == num1 + num2
+  num1 + num2
 end
-addition (num1,num2)
 
 def subtraction(num1, num2)
-subtraction == num1 - num2
+  num1 - num2
 end
-subtraction (num1,num2)
 
 def division(num1, num2)
-division == num1 / num2
+  num1 / num2
 end
-division (num1,num2)
 
 def multiplication(num1, num2)
-multiplication == num1 / num2
+  num1 * num2
 end
-multiplication (num1,num2)
 
 def modulo(num1, num2)
-modulo ==
+  num1 % num2
 end
-modulo(num1, num2)
 
 def square_root(num)
-square_root
+  Math.sqrt(num)
 end
-square_root (num1,num2)
